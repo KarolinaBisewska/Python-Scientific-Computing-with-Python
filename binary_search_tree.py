47a1d02aaa985e7dd201e144ef5ce5002946264a
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-10-15 21:47:21 +0200

    Refactor delete method in binary_search_tree.py
    Refactor delete method to improve readability and consistency.

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -44,17 +44,21 @@ class BinarySearchTree:
         if key < node.key:
             node.left = self._delete(node.left, key)
         elif key > node.key:
-            node.right = self._delete(node.right, key) 
+            node.right = self._delete(node.right, key)
         else:
             if node.left is None:
                 return node.right
             elif node.right is None:
-                return node.left    
-             
+                return node.left
+
+            node.key = self._min_value(node.right) 
+            node.right = self._delete(node.right, node.key)
+        return node
+
     def _min_value(self, node):
         while node.left is not None:
             node = node.left
-            
+        return node.key
 
 
 bst = BinarySearchTree()
