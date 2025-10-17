c5792cbba9c7d577e5487c35d10427fb402ce5d8
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-10-17 22:32:37 +0200

    Refactor R2Vector and R3Vector constructors
    Updated constructors to use keyword arguments for clarity.

--- vector_space.py
+++ vector_space.py
@@ -1,20 +1,19 @@
+
 class R2Vector:
-    def __init__(self, x, y):
+    def __init__(self, *, x, y):
         self.x = x
         self.y = y
-        
     def norm(self):
         return (self.x**2 + self.y**2)**0.5
-        
     def __str__(self):
         return f'{self.x, self.y}'
 
 class R3Vector(R2Vector):
-    def __init__(self, x, y, z):
-        super().__init__(x, y)
+    def __init__(self, *, x, y, z):
+        super().__init__(x=x, y=y)
         self.z = z
 
-v1 = R2Vector(2, 3)
-print(v1.norm())
-print(v1)
-v2 = R3Vector(2, 2, 3)
+v1 = R2Vector(x=2, y=3)
+v2 = R3Vector(x=2, y=2, z=3)
+print(v1.__dict__)
+print(v2.__dict__)
