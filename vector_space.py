e073fb861aaffd3c6b7d7ce05121c13edc1f4f69
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-10-17 22:20:55 +0200

    Implement R3Vector class with z-coordinate
    Added R3Vector class to extend R2Vector with a z-coordinate.

--- vector_space.py
+++ vector_space.py
@@ -8,8 +8,13 @@ class R2Vector:
         
     def __str__(self):
         return f'{self.x, self.y}'
+
 class R3Vector(R2Vector):
-    pass
+    def __init__(self, x, y, z):
+        super().__init__(x, y)
+        self.z = z
+
 v1 = R2Vector(2, 3)
 print(v1.norm())
 print(v1)
+v2 = R3Vector(2, 2, 3)
