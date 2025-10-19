2916d0858c560b3a4f85920ca91f87e97e87fab0
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-10-19 21:27:10 +0200

    Comment out print statements for v1 and v2
    Commented out print statements for vector instances.

--- vector_space.py
+++ vector_space.py
@@ -13,12 +13,13 @@ class R2Vector:
         arg_list = [f'{key}={val}' for key, val in vars(self).items()]
         args = ', '.join(arg_list)
         return f'{self.__class__.__name__}({args})'
-
+    
 class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
         self.z = z
+
 v1 = R2Vector(x=2, y=3)
 v2 = R3Vector(x=2, y=2, z=3)
-print(f'v1 = {v1}')
-print(f'v2 = {v2}')
+# print(f'v1 = {v1}', f'\nrepr = {repr(v1)}')
+# print(f'v2 = {v2}', f'\nrepr = {repr(v2)}')
