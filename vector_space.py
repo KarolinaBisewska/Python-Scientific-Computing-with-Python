3bd293131099198e4296fdaa69ea05e7a1b251c9
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-10-23 22:12:35 +0200

    Implement cross product method in R3Vector
    Added a cross product method to R3Vector class.

--- vector_space.py
+++ vector_space.py
@@ -58,11 +58,14 @@ class R2Vector:
 
     def __ge__(self, other):
         return not self < other
-
 class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
         self.z = z
+        
+    def cross(self, other):
+        if type(self) != type(other):
+            return NotImplemented
 v1 = R2Vector(x=2, y=3)
 v2 = R2Vector(x=0.5, y=1.25)
 print(f'v1 = {v1}')
@@ -73,4 +76,3 @@ v4 = v1 - v2
 print(f'v1 - v2 = {v4}')
 v5 = v1 * v2
 print(f'v1 * v2 = {v5}')
-print(v1 != R2Vector(x=2, y=3))
