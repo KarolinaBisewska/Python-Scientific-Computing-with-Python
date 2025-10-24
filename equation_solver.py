59051a7177113d2bf3a34b4a1c60d6da1e8e9add
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-10-24 12:07:56 +0200

    Refactor Equation class and add constructor

--- equation_solver.py
+++ equation_solver.py
@@ -1,18 +1,21 @@
 from abc import ABC, abstractmethod
-
-
 class Equation(ABC):
+    def __init__(self):
+        pass
+    
     @abstractmethod
     def solve(self):
         pass
-    @abstractmethod    
+        
+    @abstractmethod
     def analyze(self):
         pass
-        
+
+
 class LinearEquation(Equation):
     def solve(self):
         pass
+
     def analyze(self):
         pass
-
-
+lin_eq = LinearEquation()
