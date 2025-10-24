4dc5bcd16bc892f8d7e3aebadb72eeb6080d6a16
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-10-24 21:15:27 +0200

    Implement degree attribute in Equation class
    Added degree attribute and validation for subclasses.

--- equation_solver.py
+++ equation_solver.py
@@ -1,7 +1,15 @@
 from abc import ABC, abstractmethod
+
 class Equation(ABC):
-    def __init__(self):
-        pass
+    degree: int
+    def __init__(self, *args):
+        pass        
+    
+    def __init_subclass__(cls):
+        if not hasattr(cls, "degree"):
+            raise AttributeError(
+                f"Cannot create '{cls.__name__}' class: missing required attribute 'degree'"
+            )
     
     @abstractmethod
     def solve(self):
@@ -10,12 +18,14 @@ class Equation(ABC):
     @abstractmethod
     def analyze(self):
         pass
-
-
+        
 class LinearEquation(Equation):
+    degree = 1
+    
     def solve(self):
         pass
-
+    
     def analyze(self):
         pass
+    
 lin_eq = LinearEquation()
