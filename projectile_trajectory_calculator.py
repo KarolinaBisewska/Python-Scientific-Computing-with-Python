9d5b3d425d884a0d7ceb637377ff02977b2447bc
Caroline Bisewska <62181697+KarolinaBisewska@users.noreply.github.com>
2025-10-30 20:50:03 +0100

    Update projectile_trajectory_calculator.py

--- projectile_trajectory_calculator.py
+++ projectile_trajectory_calculator.py
@@ -13,15 +13,38 @@ class Projectile:
         self.__height = height
         self.__angle = math.radians(angle)
         
+    def __str__(self):
+        return f'''
+Projectile details:
+speed: {self.__speed} m/s
+height: {self.__height} m
+angle: {round(math.degrees(self.__angle))}°
+displacement: {round(self.__calculate_displacement(), 1)} m
+'''
+
     def __calculate_displacement(self):
-        v = self.__speed
+        horizontal_component = self.__speed * math.cos(self.__angle)
+        vertical_component = self.__speed * math.sin(self.__angle)
+        squared_component = vertical_component**2
+        gh_component = 2 * GRAVITATIONAL_ACCELERATION * self.__height
+        sqrt_component = math.sqrt(squared_component + gh_component)
+        
+        return horizontal_component * (vertical_component + sqrt_component) / GRAVITATIONAL_ACCELERATION
+        
+    def __calculate_y_coordinate(self, x):
+    
+        v0 = self.__speed
+        y0 = self.__height
         θ = self.__angle
-        h = self.__height
         g = GRAVITATIONAL_ACCELERATION
 
-        v_sin = v * math.sin(θ)
-        v2_sin2 = (v ** 2) * (math.sin(θ) ** 2)
-        inner_sqrt = math.sqrt(v2_sin2 + 2 * g * h)
-        time_factor = v_sin + inner_sqrt
-        d = v * math.cos(θ) * time_factor / g
-        return d
+        term1 = y0
+        term2 = x * math.tan(θ)
+        term3 = (g * x**2) / (2 * v0**2 * math.cos(θ)**2)
+
+        return term1 + term2 - term3
+
+
+ball = Projectile(10, 3, 45)
+print(ball)
+   
